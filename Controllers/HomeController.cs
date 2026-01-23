f10475c14cc2af830eb2ff85c7b08a27e484300a
marcosin78 <marcbravomadrid@gmail.com>
2026-01-23 18:45:25 +0100

    Added database in PhpMyAdmin, and functional login and registration page.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,6 +6,12 @@ namespace CompanyWebMarcBravo.Controllers;
 
 public class HomeController : Controller
 {
+    private readonly AppDbContext _context;
+
+    public HomeController(AppDbContext context)
+    {
+        _context = context;
+    }
     public IActionResult Index()
     {
         return View();
@@ -15,10 +21,33 @@ public class HomeController : Controller
     {
         return View();
     }
+    public IActionResult Register()
+    {
+        return View();
+    }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+    
+        [HttpPost]
+    public async Task<IActionResult> Register(string Username, string Password, string ConfirmPassword)
+    {
+        // Validación simple de contraseñas
+        if (Password != ConfirmPassword)
+        {
+            ViewBag.Error = "Las contraseñas no coinciden.";
+            return View();
+        }
+
+        // Crear y guardar el usuario
+        var user = new User { Username = Username, Password = Password };
+        _context.Users.Add(user);
+        await _context.SaveChangesAsync();
+
+        // Redirigir o mostrar mensaje de éxito
+        return RedirectToAction("Index");
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using CompanyWebMarcBravo.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySQL(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
